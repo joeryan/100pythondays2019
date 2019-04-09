1c4db78397ac7959b3661c8f2ceb50f99f1e3fb4
Joe Ryan <joe@clanryan.us>
2019-04-09 13:24:47 -0400

    created first database migration, Users table

--- 025/app/__init__.py
+++ 025/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 application = Flask(__name__)
 application.config.from_object(Config)
-from app import routes
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
+from app import routes, models
 
 # from app.api import bp as api_bp
 # app.register_blueprint(api_bp, url_prefix='/api')
